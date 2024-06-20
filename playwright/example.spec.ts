0e30d0d3d01849b11095312788d1b2b97d7c720d
Casper Lindberg <lindberg.casper@bcg.com>
2024-06-20 14:24:55 +0200

    Add more playwright tests

--- playwright/example.spec.ts
+++ playwright/example.spec.ts
@@ -5,4 +5,20 @@ test("has title", async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Template UI/);
+
+  // Fill out the login form
+  await page.fill('input[name="username"]', "your-username");
+  await page.fill('input[name="password"]', "your-password");
+
+  // Submit the login form
+  await page.click('button[type="submit"]');
+
+  // Check elements on the welcome page
+  await expect(page.locator("h4")).toContainText("Welcome to");
+  await expect(page.locator("h3")).toContainText("Template");
+
+  // Check elements on the sidebar
+  await expect(page.locator("text=home")).toBeVisible();
+  await expect(page.locator("text=products")).toBeVisible();
+  await expect(page.locator("text=log out")).toBeVisible();
 });
